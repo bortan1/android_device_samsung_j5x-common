aeb3e360488b9929268a35dc40b1dcdafcc4f54f
bortan1 <89531285+bortan1@users.noreply.github.com>
2025-10-16 17:01:55 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter j5xnlte j5xlte, $(TARGET_DEVICE)),)
+ifneq ($(filter j5xn3g j5x3g j5xnlte j5xlte, $(TARGET_DEVICE)),)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
