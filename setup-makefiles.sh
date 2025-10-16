6ad19b6274894fff910abcc1963d17c49019a79d
bortan1 <89531285+bortan1@users.noreply.github.com>
2025-10-16 17:04:18 +0300

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -19,7 +19,7 @@
 set -e
 
 # Required!
-export DEVICES="j5xnlte j5xlte"
+export DEVICES="j5xn3g j5x3g j5xnlte j5xlte"
 export DEVICE_COMMON=j5x-common
 export BOARD_COMMON=msm8916-common
 export VENDOR=samsung
